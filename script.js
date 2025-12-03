05d383f8745b28a3120f6d464eb3e2d23b9a48ff
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-03 13:35:36 -0700

    Fixed Issue and Button clicker Works!

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let button = document.getElementById("clicker")
-
 function clicked(){
   button.innerHTML = "clicked!"
 }
 
+const clicker = document.getElementById("button");
+const counterDisplay = document.getElementById("counterDisplay");
+let counter = 0;
+button.addEventListener("click", () => {
+  counter++;
+  counterDisplay.textContent = counter;
+});
 
-// let clickCount = 0;
-// const clickButton = document.getElementById('clickButton');
-// const clickCountDisplay = document.getElementById('clickCountDisplay');
-// clickButton.addEventListener('clicks', () => {
-//     clickCount++;
-//     clickCountDisplay.textContent = clickCount;
-// });
+
+/* <button id="clicker" onclick="clicked()">click me</button>
+<p>Clicks: <a id="clicks">0</a></p> */
+
+/* <button id="clickButton">Click Me</button>
+// <p>Clicks: <span id="counterDisplay">0</span></p> */
