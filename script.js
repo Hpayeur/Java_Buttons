fb67b9fa11047fc7993e6223d535c935801e5baf
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-02 17:58:52 -0700

    trying things out

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let button = document.getElementById("clicker")
 function clicked(){
   button.innerHTML = "clicked!"
 }
+
+
+let clickCount = 0;
+const clickButton = document.getElementById('clickButton');
+const clickCountDisplay = document.getElementById('clickCountDisplay');
+clickButton.addEventListener('clicks', () => {
+    clickCount++;
+    clickCountDisplay.textContent = clickCount;
+});
