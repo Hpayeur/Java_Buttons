deb5304430f29db5168b2dde1eee413fca2791bc
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-03 15:12:00 -0700

    Finished

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 let button = document.getElementById("clicker")
 function clicked(){
   button.innerHTML = "clicked!"
 }
+
 
 const clicker = document.getElementById("button");
 const counterDisplay = document.getElementById("counterDisplay");
@@ -12,8 +14,4 @@ button.addEventListener("click", () => {
 });
 
 
-/* <button id="clicker" onclick="clicked()">click me</button>
-<p>Clicks: <a id="clicks">0</a></p> */
 
-/* <button id="clickButton">Click Me</button>
-// <p>Clicks: <span id="counterDisplay">0</span></p> */
